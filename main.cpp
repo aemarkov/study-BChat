6cd88451fc0842254ed631f345e7230e12f4d1b1
Markov Alexey <markovalex95@gmail.com>
2017-01-07 23:38:47 +0300

    пофиксил

--- main.cpp
+++ main.cpp
@@ -2,13 +2,6 @@
 #include <QApplication>
 #include <fstream>
 
-#include "NetworkAdapter\NetworkAdapter.h"
-#include "Network\TcpListener.h"
-#include "Network\Network.h"
-#include "NetworkAdapter\NetworkProcessingThread.h"
-
-#include "crypto\cryptoapi.h"
-
 int main(int argc, char *argv[]) 
 {	
     QApplication a(argc, argv);
