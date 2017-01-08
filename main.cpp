b1014eb0fde0683a844aad5b776c3df50751a8cb
N <->
2017-01-08 05:21:46 +0300

    Добавлен автоматический лог исключений.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 #include <QApplication>
 #include <fstream>
 
-#include "util\SettingsManager\SettingsManagerContainer.h"
-using namespace Util;
-
 int main(int argc, char *argv[]) 
-{		
+{	
     QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
